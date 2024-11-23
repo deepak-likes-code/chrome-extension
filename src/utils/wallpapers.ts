35d9ecc4fabc086e041a2dd49d7c9f8bf45fbfa0
Deepak K <deepak@Deepaks-MacBook-Pro.local>
2024-11-23 21:46:02 +0530

    added some studio ghibli art work

--- src/utils/wallpapers.ts
+++ src/utils/wallpapers.ts
@@ -1,12 +1,10 @@
 export const wallpapers = [
-    "https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05",
-    "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1",
-    "https://images.unsplash.com/photo-1426604966848-d7adac402bff",
-    "https://images.unsplash.com/photo-1472214103451-9374bd1c798e",
-    "https://images.unsplash.com/photo-1490730141103-6cac27aaab94",
-    "https://images.unsplash.com/photo-1501854140801-50d01698950b",
-    "https://images.unsplash.com/photo-1441974231531-c6227db76b6e",
-    "https://images.unsplash.com/photo-1518173946687-a4c8892bbd9f",
-    "https://images.unsplash.com/photo-1475924156734-496f6cac6ec1",
+
+"https://www.ghibli.jp/gallery/karigurashi049.jpg",
+"https://www.ghibli.jp/gallery/karigurashi001.jpg",
+"https://www.ghibli.jp/gallery/chihiro003.jpg",
+"https://www.ghibli.jp/gallery/chihiro043.jpg",
+"https://www.ghibli.jp/gallery/totoro025.jpg",
+"https://www.ghibli.jp/gallery/totoro041.jpg",
     "https://images.unsplash.com/photo-1580137189272-c9379f8864fd"
   ];
